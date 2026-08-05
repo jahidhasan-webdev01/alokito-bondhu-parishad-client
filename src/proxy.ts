81d86062d23cf68a8f0040ca3540041af31e89fc
jahidhasan-webdev01 <jahidhasan.webdev01@gmail.com>
2026-08-05 13:34:48 +0600

    final

--- src/proxy.ts
+++ src/proxy.ts
@@ -4,7 +4,11 @@ export function proxy(request: NextRequest) {
   const token = request.cookies.get("accessToken");
   const pathname = request.nextUrl.pathname;
 
+  console.log("TOKEN IN PROXY", token);
+  console.log("pathname IN PROXY", pathname);
+
   if (pathname.startsWith("/dashboard") && !token) {
+    console.log("LLLLLLELELELELELLE");
     return NextResponse.redirect(new URL("/login", request.url));
   }
 
